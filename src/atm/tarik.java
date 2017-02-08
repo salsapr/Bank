80021b36c70f1f8acca5eb72b6e0f879c246d816
salsapr <salsabilaputrirayieningtyas@gmail.com>
2017-02-08 08:15:15 +0700

    pembuatan sistem ATM

---
+++ src/atm/tarik.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atm;
+
+/**
+ *
+ * @author Salsabila Putri Ray
+ */
+import javax.swing.JOptionPane;
+public class tarik {
+    private int saldo;
+    public void ambil (int tab, int t){
+    if(t%100!=0)
+        JOptionPane.showMessageDialog(null, "Mesin ATM BRI tidak menerima uang koin \nSilahkan ulangi lagi", "Transaksi Gagal",0);
+    else{
+        if(t>tab)
+            JOptionPane.showMessageDialog(null, "Saldo Anda tidak mencukupi \nSilahkan Lakukan penyetoran","Transaksi Gagal",0);
+        else if (t<50000)
+            JOptionPane.showMessageDialog(null, "Besaran minimal pengambilan tunai adalah Rp50000","Transaksi Gagal",0);
+        else{
+           saldo = tab-t;
+           JOptionPane.showMessageDialog(null,"Saldo Anda saat ini sebesar :"+saldo);
+           if (tab<=50000)
+               JOptionPane.showMessageDialog(null,"Saldo minimal harus Rp 50000,00 \nSegera lakukan penyetoran untuk menghindari penutupan akun.","Caution",2);
+        }
+    }
+    }
+    public int getsaldo(){
+        return saldo;
+    }
+}
